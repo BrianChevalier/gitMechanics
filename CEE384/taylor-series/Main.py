3138a76d39ad99f755344dfcdf184d8aacded9f1
Brian Badahdah <adambadahdah@gmail.com>
2018-12-12 14:21:47 -0700

    Updated taylor series plots

--- CEE384/taylor-series/Main.py
+++ CEE384/taylor-series/Main.py
@@ -10,10 +10,30 @@ plt.xlabel('x')
 plt.ylabel('f(x)')
 x = np.linspace(-np.pi, np.pi, 500)
 
-plt.plot(x, -1/6*x**3 + x, label='Taylor Polynomial')
+plt.plot(x, -1/6*x**3 + x, label='3rd order Taylor Polynomial',lw=2, linestyle='--',color='green')
 
-plt.plot(x, np.sin(x), label='sin(x)')
+plt.plot(x, np.sin(x), label='sin(x)',color='black',lw=3)
 
 plt.legend()
-
 f.savefig("Figures/3ordersin.pdf", bbox_inches='tight')
+
+## Multi taylor
+f = plt.figure()
+plt.grid(True, which='both', linestyle='dashed')
+plt.xlabel('x')
+plt.ylabel('f(x)')
+x = np.linspace(-np.pi, np.pi, 500)
+
+#5th
+plt.plot(x, x**5/120 - 1/6*x**3 + x, label='5th order',lw=2, linestyle='--')
+
+#3rd
+plt.plot(x, -1/6*x**3 + x, label='3rd order',lw=2, linestyle='--')
+#1st
+plt.plot(x, x, label='1st order',lw=2, linestyle='--')
+
+#True
+plt.plot(x, np.sin(x), label='sin(x)',color='black',lw=3)
+
+plt.legend()
+f.savefig("Figures/135ordersin.pdf", bbox_inches='tight')
